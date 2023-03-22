4fe4a2f1c0e53544a0f261c8222dda4fbf117c5e
Santiago Rodriguez <05rmsantiago@gmail.com>
2023-03-22 00:19:18 -0300

    chore: add auth and bd

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,12 @@
+const SideBar = () => {
+  return (
+    <aside className="absolute top-[10%] left-[2%] h-[50rem] w-52 rounded-xl bg-gray-300">
+      <span>Parametros</span>
+      <div>
+        <ul></ul>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
